f624cb923bfe6ae54fd56d4211df661ecda72d4e
Ivokator <randomjw3@gmail.com>
2025-02-13 15:00:47 -0800

    made king card bigger or something

--- main.py
+++ main.py
@@ -1,22 +1,70 @@
 OLED12864_I2C.init(60)
-
+OLED12864_I2C.zoom(False)
+OLED12864_I2C.clear()
 
 
 def king_card(position = 0):
-    OLED12864_I2C.rect((position*10), 0, (position*10)+ 8, 11, 1)
-    OLED12864_I2C.vline(2, 3, 5, 1)
+    OLED12864_I2C.rect((position*20), 0, (position*20) + 16, 22, 1)
 
-    OLED12864_I2C.pixel(3, 5, 1)
-    OLED12864_I2C.pixel(4, 4, 1)
-    OLED12864_I2C.pixel(4, 6, 1)
+    # Making the 'K'
+    OLED12864_I2C.vline((position*20) + 3, 4, 13, 3)
+    
+    # Diagonal lines
+    for i in range(7):
+        OLED12864_I2C.pixel((position*20) + 4 + i, 10 - i, 1)
+    for i in range(7):
+            OLED12864_I2C.pixel((position*20) + 4 + i, 10 + i, 1)
 
-    OLED12864_I2C.pixel(5, 3, 1)
-    OLED12864_I2C.pixel(5, 7, 1)
 
-OLED12864_I2C.zoom(False)
+def queen_card(position = 0):
+    OLED12864_I2C.rect((position*20), 0, (position*20) + 16, 22, 1)
 
-OLED12864_I2C.show_string(0, 0, "A 7 6 10 8", 1)
-OLED12864_I2C.show_string(0, 3, "K A 9 10 8", 1)
+    # Making the 'Q'
+    OLED12864_I2C.vline((position*20) + 3, 4, 13, 3)
+    
+    for i in range(7):
+        OLED12864_I2C.pixel((position*20) + 4 + i, 10 - i, 1)
+    for i in range(7):
+            OLED12864_I2C.pixel((position*20) + 4 + i, 10 + i, 1)
+
+
+OLED12864_I2C.show_number(0, 0, 12, 1)
+
+
+pins.digital_write_pin(DigitalPin.P3, 0)
+
+
+toggle = 0
+
+def on_button_pressed_b():
+    global toggle
+    if toggle == 0:
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+        toggle = 1
+    elif toggle == 1:
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+        toggle = 2
+    elif toggle == 2:
+        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.digital_write_pin(DigitalPin.P3, 1)
+        toggle = 0
+    print(toggle)
+# input.on_button_pressed(Button.B, on_button_pressed_b)
+
+
+
+#OLED12864_I2C.show_string(0, 0, "A 7 6 20 8", 1)
+#OLED12864_I2C.show_string(0, 3, "K A 9 20 8", 1)
+
+
+
+
+
 
 def win_round():
     music.play(music.tone_playable(Note.C, music.beat(BeatFraction.WHOLE)), music.PlaybackMode.UNTIL_DONE)
--- main.ts
+++ main.ts
@@ -1,17 +1,61 @@
 OLED12864_I2C.init(60)
+OLED12864_I2C.zoom(false)
+OLED12864_I2C.clear()
 function king_card(position: number = 0) {
-    OLED12864_I2C.rect(position * 10, 0, position * 10 + 8, 11, 1)
-    OLED12864_I2C.vline(2, 3, 5, 1)
-    OLED12864_I2C.pixel(3, 5, 1)
-    OLED12864_I2C.pixel(4, 4, 1)
-    OLED12864_I2C.pixel(4, 6, 1)
-    OLED12864_I2C.pixel(5, 3, 1)
-    OLED12864_I2C.pixel(5, 7, 1)
+    let i: number;
+    OLED12864_I2C.rect(position * 20, 0, position * 20 + 16, 22, 1)
+    //  Making the 'K'
+    OLED12864_I2C.vline(position * 20 + 3, 4, 13, 3)
+    //  Diagonal lines
+    for (i = 0; i < 7; i++) {
+        OLED12864_I2C.pixel(position * 20 + 4 + i, 10 - i, 1)
+    }
+    for (i = 0; i < 7; i++) {
+        OLED12864_I2C.pixel(position * 20 + 4 + i, 10 + i, 1)
+    }
 }
 
-OLED12864_I2C.zoom(false)
-OLED12864_I2C.showString(0, 0, "A 7 6 10 8", 1)
-OLED12864_I2C.showString(0, 3, "K A 9 10 8", 1)
+function queen_card(position: number = 0) {
+    let i: number;
+    OLED12864_I2C.rect(position * 20, 0, position * 20 + 16, 22, 1)
+    //  Making the 'Q'
+    OLED12864_I2C.vline(position * 20 + 3, 4, 13, 3)
+    for (i = 0; i < 7; i++) {
+        OLED12864_I2C.pixel(position * 20 + 4 + i, 10 - i, 1)
+    }
+    for (i = 0; i < 7; i++) {
+        OLED12864_I2C.pixel(position * 20 + 4 + i, 10 + i, 1)
+    }
+}
+
+OLED12864_I2C.showNumber(0, 0, 12, 1)
+pins.digitalWritePin(DigitalPin.P3, 0)
+let toggle = 0
+function on_button_pressed_b() {
+    
+    if (toggle == 0) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        toggle = 1
+    } else if (toggle == 1) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        toggle = 2
+    } else if (toggle == 2) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        toggle = 0
+    }
+    
+    console.log(toggle)
+}
+
+//  input.on_button_pressed(Button.B, on_button_pressed_b)
+// OLED12864_I2C.show_string(0, 0, "A 7 6 20 8", 1)
+// OLED12864_I2C.show_string(0, 3, "K A 9 20 8", 1)
 function win_round() {
     music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 }
